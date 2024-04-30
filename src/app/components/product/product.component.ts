44d668702c67d2c0293d8a5d95bed2de1a7160d7
zulalkaradepe <zulalkaradepe12@gmail.com>
2024-05-01 00:02:58 +0300

    ilk commit

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent implements OnInit{
+
+  products:Product[] = []
+  dataLoaded = false
+
+constructor(private productService:ProductService){ }
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
+
+
+getProducts(){
+
+  this.productService.getProducts().subscribe(response=>
+    {
+    this.products = response.data
+    this.dataLoaded = true
+  })
+  
+}
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { ListResponseModel } from '../models/listResponseModel';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  apiUrl = 'https://localhost:44359/api/products/getall'
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<ListResponseModel<Product>> {
+
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl)
+    
+  }
+
+}
